2128a8b805399cf44152851a12ff4b9242ac0c0d
abdiwakene <63557118+abdiwakene@users.noreply.github.com>
2021-12-07 12:11:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// init project
+const express = require("express"); // the library we will use to handle requests
+const mongodb = require("mongodb"); // load mongodb
+const urls = require("url");
+
+const port = process.env.port || 8080; // port to listen on
+
+const app = express(); // instantiate express
+//app.use(require("cors")()); // allow Cross-domain requests
+//app.use(require("body-parser").json()); // automatically parses request data to JSON
+//app.use(express.urlencoded());
+
+
+const uri = "mongodb+srv://atlasAdmin:cs20password@cluster0.nhrgx.mongodb.net/companies?retryWrites=true&w=majority"; // put your URI HERE
+
+
+mongodb.MongoClient.connect(uri, (err, db) => {
+  // connect to our database
+  const dbo = db.db("companies");
+  const collection = dbo.collection("companies");
+
+  // Responds to GET requests with the route parameter being the form.
+  app.get("/:company", (req, res) => {
+    var qobj = urls.parse(req.url, true).query
+    var querys;
+    if (qobj.name != '') {
+        //name query 
+        querys = {'name' : qobj.name};
+    } else if (qobj.ticker != '') {
+        //ticker query
+        querys = {'ticker': qobj.ticker};
+    } 
+    // search the database (collection) for all users with the `user` field being the `user` route paramter
+    collection.find(querys).toArray(async (err, docs) => {
+      if (err) {
+        // if an error happens
+        res.send("Error in GET req.");
+      } else {
+        // if all works
+        var results = "";
+        await docs.forEach(function(item){
+            results += item.name + " : " + item.ticker + "\n"; // send back all users found with the matching username
+        });
+        if (docs.length === 0) {
+          results = "No Match Found!";
+        }
+        res.end(results);
+      }
+    });
+  });
+
+  // listen for requests
+  var listener = app.listen(port, () => {
+    console.log("This app is listening on port " + listener.address().port);
+  });
+});
